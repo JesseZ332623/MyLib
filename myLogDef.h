4b105f10d2a150a9032e19dc59b3fbbaece6a3a2
Jesse <3191955858@qq.com>
2024-03-27 15:30:24 +0800

    一个自定义的简易日志库，能完成基本的日志功能

--- myLogDef.h
+++ myLogDef.h
@@ -1,60 +1,82 @@
 #ifndef _TERMINAL_COLOR_H_
 #define _TERMINAL_COLOR_H_
 
-#include <ostream>
+#include <iostream>
 
-enum ColorMenu { RED = 1, GREEN, YELLO, BLUE = 6, WHITE };
-
-class TerminalTextColor
+namespace MyLog
 {
-    private:
-        int colorCode {0};
 
-    public:
-        TerminalTextColor(const int __colorSign) : colorCode(__colorSign) {}
+    enum ColorMenu { RED = 1, GREEN, YELLO, BLUE = 6, WHITE };
 
-        friend std::ostream & operator<<(std::ostream & __os, const TerminalTextColor & __colorSign)
-        {
-            std::string asciiColorSign;
+    class TerminalTextColor
+    {
+        private:
+            int colorCode {0};
 
-            asciiColorSign += "\033[38;5;" + std::to_string(__colorSign.colorCode) + "m";
+        public:
+            TerminalTextColor(const int __colorSign) : colorCode(__colorSign) {}
 
-            __os << asciiColorSign; 
+            friend std::ostream & operator<<(std::ostream & __os, const TerminalTextColor & __colorSign)
+            {
+                std::string asciiColorSign;
 
-            return __os;
-        }
-};
+                asciiColorSign += "\033[38;5;" + std::to_string(__colorSign.colorCode) + "m";
 
-typedef struct LogLevel
-{
-    public:
-        TerminalTextColor Original{WHITE};
-        TerminalTextColor Notify{BLUE};
-        TerminalTextColor Warning{YELLO};
-        TerminalTextColor Correct{GREEN};
-        TerminalTextColor Error{RED};
-} MyLog;
+                __os << asciiColorSign; 
 
-/*一个简单的日志库，用于不同类型消息的输出*/
-const MyLog myLog;
+                return __os;
+            }
+    };
 
-#define ORIGINAL    myLog.Original
-#define NOTIFY      myLog.Notify
-#define WARNING     myLog.Warning
-#define CORRECT     myLog.Correct
-#define ERROR       myLog.Error
+    typedef struct LogLevel
+    {
+        public:
+            TerminalTextColor Original{WHITE};
+            TerminalTextColor Notify{BLUE};
+            TerminalTextColor Warning{YELLO};
+            TerminalTextColor Correct{GREEN};
+            TerminalTextColor Error{RED};
+    } MyLog;
 
-/**
-* @brief            这个函数可以打印不同风格的分割线
-* 
-* @param __len      分割线的长度
-* @param __style    分割线的风格，如（------- ****** 或 ·········）
-* 
-* @return           non-return
-*/
-static inline void printSplitLine(int __len, const char __style)
-{
-    std::cout << std::string(__len, __style) << '\n';
+    /*一个简单的日志库，用于不同类型消息的输出*/
+    const MyLog myLog;
+
+    #define ORIGINAL    myLog.Original
+    #define NOTIFY      myLog.Notify
+    #define WARNING     myLog.Warning
+    #define CORRECT     myLog.Correct
+    #define ERROR       myLog.Error
+
+    /**
+     * @brief 用户可以通过该函数简便的使用该日志库
+     * 
+     * @tparam OutPutType 需要输出的类型
+     * 
+     * @param __logLevel    日志等级
+     * @param __out         要输出的数据
+     * @param __os          标准输出流
+     * 
+     * @return              non-return
+    */
+    template <typename OutPutType>
+    static inline void log(const TerminalTextColor & __logLevel, const OutPutType & __out, std::ostream & __os = std::cout)
+    {
+        __os << __logLevel << __out << ORIGINAL << std::endl;
+    }
+
+    /**
+    * @brief            这个函数可以打印不同风格的分割线
+    * 
+    * @param __len      分割线的长度
+    * @param __style    分割线的风格，如（------- ****** 或 ·········）
+    * @param __os       标准输出流
+    * 
+    * @return           non-return
+    */
+    static inline void printSplitLine(int __len, const char __style, std::ostream & __os = std::cout)
+    {
+        __os << std::string(__len, __style) << '\n';
+    }
 }
 
 #endif  // _TERMINAL_COLOR_H_
