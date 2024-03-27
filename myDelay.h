ba2ac9a4ef9c1bec517e006037d3366215dfa247
Jesse <3191955858@qq.com>
2024-03-27 15:39:46 +0800

    一个简易的延时库，但是在 Linux 系统上似乎无效

--- myDelay.h
+++ myDelay.h
@@ -3,20 +3,23 @@
 
 #include <time.h>
 
-/**
- * @brief 自制延时函数，用于代替 windows 库中的 Sleep()
- * 
- * @param __millisSeconds 延时时间（毫秒）
- * 
- * @return non-return
-*/
-static void delay(long int __millisSeconds);
-
-static void delay(long int __millisSeconds)
+namespace MyDelay
 {
-    clock_t startTime = clock();
- 
-    while (clock() < (startTime + __millisSeconds)) {}
+    /**
+     * @brief 自制延时函数，用于代替 windows 库中的 Sleep()
+     * 
+     * @param __millisSeconds 延时时间（毫秒）
+     * 
+     * @return non-return
+    */
+    static void delay(long int __millisSeconds);
+
+    static void delay(long int __millisSeconds)
+    {
+        clock_t startTime = clock();
+    
+        while (clock() < (startTime + __millisSeconds)) {}
+    }
 }
 
 #endif // _MY_SLEEP_H_
