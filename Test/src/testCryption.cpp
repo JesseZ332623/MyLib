15f0b6af414ba59a1c1fcdea66cb0f049d7b13e2
浩进 朱 <3191955858@qq.com>
2024-08-09 20:26:03 +0800

    AES 加密解密的测试用例

--- Test/src/testCryption.cpp
+++ Test/src/testCryption.cpp
@@ -8,10 +8,10 @@ int main(int argc, char const *argv[])
 {
     using namespace MyLib::MyLoger;
 
-    Cryption testCryption;
-    //std::string plainText = "这是一个危险的世界，隐藏自己，做好清理！！";
+    Cryption testCryption("../data/key.dat");
+    std::string plainText = "这是一个危险的世界，隐藏自己，做好清理！！";
 
-    std::string cipherText = testCryption.encryption("这是一个危险的世界，隐藏自己，做好清理！！");
+    std::string cipherText = testCryption.encryption("这是一个危险的世界，隐藏自己，做好清理！!");
 
     ERROR_LOG(cipherText + '\n');
 
